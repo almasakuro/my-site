3ae76d1f14987e094e7b154423e83995d6d6a93e
Almas <akzigitovalmas04@gmail.com>
2026-08-07 04:44:09 +0500

    Configure basic templates and styles

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,11 @@ const pugPlugin = require("@11ty/eleventy-plugin-pug").default;
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pugPlugin);
   eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  eleventyConfig.addWatchTarget("src/css/");
 
   return {
     dir: { input: "src", output: "_site" },
