e9b0e93b1f4876f92c4449dbb7868a94a2c90471
Almas <akzigitovalmas04@gmail.com>
2026-08-08 04:07:10 +0500

    Add design section with auto-generated project list

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
 const pugPlugin = require("@11ty/eleventy-plugin-pug").default;
 
 module.exports = function (eleventyConfig) {
@@ -5,9 +8,23 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/fonts");
-  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/**/*.{png,jpg,jpeg,gif,svg,webp,avif,mp4,webm,mov}");
 
   eleventyConfig.addWatchTarget("src/css/");
+
+  eleventyConfig.addCollection("projects", (api) =>
+    api
+      .getFilteredByGlob("./src/design/*/index.pug")
+      .map((project) => {
+        const dir = path.dirname(project.inputPath);
+        const cover = fs.readdirSync(dir).find((f) => f.startsWith("cover."));
+
+        project.data.cover = cover ? project.url + cover : null;
+
+        return project;
+      })
+      .sort((a, b) => b.date - a.date)
+  );
 
   return {
     dir: { input: "src", output: "_site" },
